2c942615998ada2f5a8335001f6711f726dccd7d
Jakub Tůma <st026360@student.cichnovabrno.cz>
2026-06-10 19:28:52 +0200

    Update Program.cs

--- moje_kody/Student/Program.cs
+++ moje_kody/Student/Program.cs
@@ -1,1 +1,12 @@
+class Program
+{
+    static void Main()
+    {
+        Student Jakub = new Student("Jakub", 18, "IT2B");
 
+        Jakub.Znamka(2);
+        Jakub.Znamka(1);
+
+        Console.WriteLine($"Jmeno: {Jakub.Jmeno} | Vek: {Jakub.Vek} | Trida: {Jakub.Trida} | Znamky: {string.Join(", ", Jakub.Znamky)}");
+    }
+}
