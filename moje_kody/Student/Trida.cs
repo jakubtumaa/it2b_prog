73f376f9021285eb087e6820e48abb672673e467
Jakub Tůma <st026360@student.cichnovabrno.cz>
2026-06-10 19:29:15 +0200

    Update Trida.cs

--- moje_kody/Student/Trida.cs
+++ moje_kody/Student/Trida.cs
@@ -1,1 +1,26 @@
+public class Student
+{
+    public string Jmeno { get; set; }
+    public int Vek { get; set; }
+    public string Trida { get; set; }
+    public List<int> Znamky { get; set; }
 
+
+    public Student (string jmeno, int vek, string trida)
+    {
+        Jmeno = jmeno;
+        Vek = vek;
+        Trida = trida;
+        Znamky = new List<int>();
+    }
+
+    public void Znamka(int znamka)
+    {
+        Znamky.Add(znamka);
+    }
+
+    public void ZmenaTridy(string novaTrida)
+    {
+        Trida = novaTrida;
+    }
+}
