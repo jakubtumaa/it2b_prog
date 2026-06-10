f800211e6f202949001124cf0869db17f7bb8189
Jakub Tůma <st026360@student.cichnovabrno.cz>
2026-06-10 19:25:17 +0200

    Update Program.cs

--- moje_kody/Hra/Program.cs
+++ moje_kody/Hra/Program.cs
@@ -1,1 +1,25 @@
+class Program
+{
+    static void Main()
+    {
+        Hra Jakub = new Hra("Jakub", 10, 0);
 
+        Jakub.PridejPredmet("Mec");
+        Jakub.PridejPredmet("Helma");
+        Jakub.LevelUp(3);
+        Jakub.VypisInfo();
+
+        Hra Filip = new Hra("Filip", 10, 0);
+
+        Filip.PridejPredmet("Mec");
+        Filip.PridejPredmet("Helma");
+        Filip.PridejPredmet("Lek");
+        Filip.LevelUp(3);
+        Filip.VypisInfo();
+
+        Jakub.Utok(Filip, 2);
+        Filip.VypisInfo();
+        Filip.Vyleceni(Filip, 2);
+        Filip.VypisInfo();
+    }
+}
